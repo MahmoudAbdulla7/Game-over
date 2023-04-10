5014ea5cfb7bbd484eb0c4c7396a012973698981
Mahmoud Abdullah <116966873+MahmoudAbdulla7@users.noreply.github.com>
2023-04-10 03:17:37 +0200

    Update Error.jsx

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -15,7 +15,7 @@ export default function Error() {
       <h2 className="mt-4 font-bold fs-1">Page not found</h2>
       <p className="mt-4 text-muted">Sorry, we couldn’t find the page you’re looking for.</p>
       <div className="flex items-center justify-center">
-        <Link to='home' className="btn btn-outline-info text-light">Go back home</Link>
+        <Link to='/' className="btn btn-outline-info text-light">Go back home</Link>
       </div>
     </div>
   </main>
